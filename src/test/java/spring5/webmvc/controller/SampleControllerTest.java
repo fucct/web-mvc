63b93780505310d165199c03cbf92f3c990d3fa0
dd <dqrd123@gmail.com>
2020-04-26 22:04:30 +0900

    test, feat : 다양한 Mapping Test

---
+++ src/test/java/spring5/webmvc/controller/SampleControllerTest.java
@@ -0,0 +1,52 @@
+package spring5.webmvc.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void get1() throws Exception {
+        mockMvc.perform(get("/events"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    void get2(Integer id) throws Exception {
+        mockMvc.perform(get("/events/" + id))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello "+id));
+    }
+
+    @Test
+    void post1() throws Exception {
+        mockMvc.perform(post("/events")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    void delete1(Integer id) throws Exception {
+        mockMvc.perform(delete("/events/" + id))
+                .andExpect(status().isOk())
+                .andExpect(content().string("delete " + id));
+    }
+
+}
