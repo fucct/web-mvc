33013dbd0fbe36e71027939c0261fb3a3d5e3cc4
dd <dqrd123@gmail.com>
2020-04-27 21:29:29 +0900

    feat : @ModelAttribute

--- src/main/java/spring5/webmvc/controller/EventController.java
+++ src/main/java/spring5/webmvc/controller/EventController.java
@@ -4,13 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import spring5.webmvc.service.EventService;
+
+import java.util.Arrays;
 
 @Controller
 public class EventController {
 
     @Autowired
     private EventService eventService;
+
+    @ModelAttribute
+    public void categories(Model model){
+        model.addAttribute("categories", Arrays.asList("a", "b", "c", "d"));
+    }
 
     @GetMapping("/events")
     public String events(Model model) {
